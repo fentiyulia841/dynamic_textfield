2313598d23fb22386217f3c4b133a9b697d004d9
fentiyulia841 <fentikristanti@gmail.com>
2022-05-19 08:55:59 +0800

    first commit

---
+++ lib/person_entry_model.dart
@@ -0,0 +1,11 @@
+class PersonEntry {
+  final String name;
+  final String age;
+  final String studyJob;
+
+  PersonEntry(this.name, this.age, this.studyJob);
+  @override
+  String toString() {
+    return 'Person: name= $name, age= $age, study job= $studyJob';
+  }
+}
